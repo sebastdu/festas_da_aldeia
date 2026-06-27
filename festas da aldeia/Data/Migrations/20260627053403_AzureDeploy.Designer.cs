ef7063aa553ea6433ce0f15842767c730528913b
Jose Martins <aluno24269@ipt.pt>
2026-06-27 16:59:01 +0100

    azure

--- festas da aldeia/Data/Migrations/20260627053403_AzureDeploy.Designer.cs
+++ festas da aldeia/Data/Migrations/20260627053403_AzureDeploy.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using festas_da_aldeia.Data;
 
@@ -11,9 +12,11 @@ using festas_da_aldeia.Data;
 namespace festas_da_aldeia.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260627053403_AzureDeploy")]
+    partial class AzureDeploy
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
